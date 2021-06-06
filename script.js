8b17527072493038403c64f556cd80e3ede53823
hunterD3CD <hunterd3c@gmail.com>
2021-06-06 14:07:01 -0700

    set variables for current time, dates, and descirption box for each time slot

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+// ------------------------------------ESTABLISHING VARIABLES-----------------------------------
+// variable: current day
+var dayE1 = $("#currentDay");
+// variable: description for each time
 var div9 = $("#div9");
-
-$("#btn9").click(function () {
-  div9.hide();
-});
+var div10 = $("#div10");
+var div11 = $("#div11");
+var div12 = $("#div12");
+var div13 = $("#div13");
+var div14 = $("#div14");
+var div15 = $("#div15");
+var div16 = $("#div16");
+var div17 = $("#div17");
+// variable: save buttons
+var btn9 = $("#btn9");
+var btn10 = $("#btn10");
+var btn11 = $("#btn11");
+var btn12 = $("#btn12");
+var btn13 = $("#btn13");
+var btn14 = $("#btn14");
+var btn15 = $("#btn15");
+var btn16 = $("#btn16");
+var btn17 = $("#btn17");
+// variable: current time
+var timeNow = parseInt(moment().format("HH"));
+console.log(timeNow);
